f8539fc00bcca7ac61b9ea8a2765e9fa8d65661e
anelizab <anelizab@db.tec.br>
2024-01-30 16:33:23 -0300

    feat: uso de váriável

--- cypress/e2e/Mentoria/games.cy.js
+++ cypress/e2e/Mentoria/games.cy.js
@@ -1,13 +1,13 @@
-// No início do seu arquivo de teste ou configuração
-//require('dotenv').config();
+// No início do seu arquivo de teste ou configuração, para carregar as variáveis de ambiente no código
+require('dotenv').config();
 
 // Exemplo de utilização das variáveis
-//const baseUrl = Cypress.env('BASE_URL');
+const baseUrl = Cypress.env('BASE_URL');
 
 
 describe('Testes utilizando variáveis de ambiente', () => {
     it('Acessar pagina', () => {
-        cy.visit('https://www.gamestolearnenglish.com/');
+        cy.visit(baseUrl);
       });
 
 });
