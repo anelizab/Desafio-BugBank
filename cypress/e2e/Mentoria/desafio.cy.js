c24e48b429338c10f760ed48bd6dc46d3176b728
anelizab <anelizab@db.tec.br>
2024-03-05 18:29:16 -0300

    feat: atualização no desafio

--- cypress/e2e/Mentoria/desafio.cy.js
+++ cypress/e2e/Mentoria/desafio.cy.js
@@ -1,3 +1,7 @@
+//import '@testing-library/cypress/add-commands';
+import '@testing-library/dom';
+import '@testing-library/cypress';
+
 describe('Desafio bugbank', () => {
     beforeEach(() => {
         cy.visit('https://bugbank.netlify.app/');
@@ -6,23 +10,19 @@ describe('Desafio bugbank', () => {
     it('Login ', () => {
        cy.get('.ihdmxA').click();
        cy.wait(500);
-       cy.get('[name="email"]').should("exist");
-       //cy.get('input[name="email"]').type('anelizafb@gmail.com');
-    
-    //cy.get('[name="email"]').click({force: true});
-    //cy.get(':nth-child(2) > .input__default').type("anelizafb@gmail.com");
-    //cy.get('[name="name"]').type("Aneliza");
-    //cy.get('[name="password"]').type("123456");
-    //cy.get('[name="passwordconfirmation"]').type("123456");
-    // //input[@name="name"]
-    
-    //login
-    //cy.get('input[name="email"]').should("have.value","");
-    //cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type("anelizafb@gmail.com");
-    //cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default'). type("123456");
-         
-       
-  
+       cy.get(':nth-child(2) > .input__default').click({force:true}).type('anelizafb@gmail.com').should('exist')
+       cy.get(':nth-child(3) > .input__default').click({force:true}).type('Aneliza').should('exist')
+       cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').click({force:true}).type('123456').should('exist')
+       cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').click({force:true}).type('123456').should('exist')
+       cy.get('#toggleAddBalance').click({force:true})  // TEM OUTRA MANEIRA DE FAZER????
+       cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({force:true})
+       cy.get('#modalText').should("exist")
+       cy.get('#btnCloseModal').click()
+       cy.get('input[name="email"]').should("have.value","");
+       cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type("anelizafb@gmail.com");
+       cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default'). type("123456");       
+       cy.get('.otUnI').click();
+       cy.contains('.home__ContainerText-sc-1auj767-7 > :nth-child(2)', 'bem vindo ao BugBank :)').should('exist')
     })
 
 })
---
+++ cypress/e2e/Mentoria/exemplos.cy.js
@@ -0,0 +1,14 @@
+    //cy.get('input[placeholder="Informe seu e-mail"]').should("exist");
+    //cy.findByText('Cadastrar').click();
+    //cy.get('input[name="email"].input__default').type('anelizafb@gmail.com', { force: true });
+    //cy.get('input[name="email"]').first().type('anelizafb@gmail.com');
+    //cy.get('input[name="email"]').eq(0).type('anelizafb@gmail.com');
+    //cy.get('input[name="email"].input__default').type('anelizafb@gmail.com');
+    //cy.get('input[name="email"]').type('anelizafb@gmail.com');
+    //cy.contains(".input__label", "E-mail");
+    //cy.get('[name="email"]').click({force: true});
+    //cy.get(':nth-child(2) > .input__default').type("anelizafb@gmail.com");
+    //cy.get('[name="name"]').type("Aneliza");
+    //cy.get('[name="password"]').type("123456");
+    //cy.get('[name="passwordconfirmation"]').type("123456");
+    // //input[@name="name"]
