9252c4b3b55c5465ad68e64452b6e714e9ceb0ef
anelizab <anelizab@db.tec.br>
2024-03-14 17:40:10 -0300

    feat: Atualizacao do desafio

---
+++ cypress/e2e/Mentoria/bugbankTransferencia.cy.js
@@ -0,0 +1,39 @@
+import '@testing-library/dom';
+import '@testing-library/cypress';
+
+describe('Desafio bugbank', () => {    
+    it('Registrar usuário ', () => {
+       cy.visit('https://bugbank.netlify.app/');
+       cy.get('.ihdmxA').click();
+       cy.wait(500);
+       cy.get(':nth-child(2) > .input__default').click({force:true}).type('anelizafb@gmail.com').should('exist')
+       cy.get(':nth-child(3) > .input__default').click({force:true}).type('Aneliza').should('exist')
+       cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').click({force:true}).type('123456').should('exist')
+       cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').click({force:true}).type('123456').should('exist')
+       cy.get('#toggleAddBalance').click({force:true})  // TEM OUTRA MANEIRA DE FAZER????
+       cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({force:true})
+       cy.get('#modalText').should("exist")
+       cy.get('#btnCloseModal').click()
+       cy.get('input[name="email"]').should("have.value","");
+       cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type("anelizafb@gmail.com");
+       cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default'). type("123456");       
+       cy.contains('button', 'Acessar').click();
+       cy.contains('.home__ContainerText-sc-1auj767-7 > :nth-child(2)', 'bem vindo ao BugBank :)').should('exist')
+       cy.get('#textAccountNumber > span').should('exist')
+       
+       const accountNumberElement = cy.findByText('Conta digital:');
+       accountNumberElement.invoke('text').then((accountNumber) => {
+         cy.task('getClipboard').then(async () => {
+           await clipboardy.writeSync(accountNumber);
+         });
+         cy.paste().should('equal', accountNumber);
+       });
+
+    }) 
+})
+
+        // DESAFIO
+        // criar uma conta, guardar as informações da conta criada e sair
+        // criar a segunda conta 
+        // realizar a tranferência de valor para a primeira conta
+        // validar o saldo
---
+++ cypress/e2e/Mentoria/exemplosparaEstudo.cy.js
@@ -0,0 +1,32 @@
+      //vai interceptar uma requisição (onde busca alguma informção no back), e depois aguardar a requisição terminar
+    // cy.intercept('**/notes').as('getnotes')  
+    // cy.wait('@getNotes')    
+
+
+    it('copies input field text to the clipboard', () => {
+      cy.visit('./index.html')
+    
+      cy.contains('button', 'Copy to clipboard')
+        .focus()
+        .click()
+    
+      cy.assertValueCopiedToClipboard('Hello World')
+    
+      cy.get('#copy-to-clipboard-input-field')
+        .clear()
+        .type('Foo bar baz')
+    
+      cy.contains('button', 'Copy to clipboard')
+        .focus()
+        .click()
+    
+      cy.assertValueCopiedToClipboard('Foo bar baz')
+    })
+    
+    Cypress.Commands.add('assertValueCopiedToClipboard', value => {
+      cy.window().then(win => {
+        win.navigator.clipboard.readText().then(text => {
+          expect(text).to.eq(value)
+        })
+      })
+    })
